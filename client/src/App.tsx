ffb9570e2c5cf071c6e11e629cfc5a782e87b148
amir-kedis <amir.kedis@gmail.com>
2024-07-01 14:57:22 +0300

    chore: installing deps

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>
-        Count: {count}
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-      </h1>
+      <h1 className="text-5xl text-zinc-500">Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
   );
 }
